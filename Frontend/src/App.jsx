09814291229c18d0163c9e1375562023a0509198
Anurag Kumar <131742834+anuragkumar1903@users.noreply.github.com>
2024-12-19 23:01:20 +0530

    updated format of code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react';
+
 import './app.css';
+import Home from './components/Pages/Home';
 import {
-  createBrowserRouter,
+
   Route,
-  RouterProvider,
+
   Routes
 } from "react-router-dom";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 
 function App() {
-  
+
 
   return (
-        <>
-          <Header />
-          <Footer />
-        </>
-        //  <Routes>
-        //     <Route path="/" element={<Home />} />
-        //     <Route path="Header" element={<Header/>} />
-        //  </Routes>       
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/pharmacy' element={<pharmacy/>} />
+
+    </Routes>
   );
 }
 
