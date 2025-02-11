3c422b57703ac4adc5c1fd89afbf9adf88954bbd
TanisqaRaj <rajtanisqa@gmail.com>
2025-02-11 23:56:56 +0530

    Error

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,9 +8,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <Provider store={store}> 
       <App />
     </Provider>
-  </StrictMode>
+
 );
