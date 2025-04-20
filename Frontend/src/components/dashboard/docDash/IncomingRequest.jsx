2d08a39ec245abf5ac76934bfd636fdda4071460
Anurag Kumar <131742834+anuragkumar1903@users.noreply.github.com>
2025-04-20 22:10:16 +0530

    update inq req

--- Frontend/src/components/dashboard/docDash/IncomingRequest.jsx
+++ Frontend/src/components/dashboard/docDash/IncomingRequest.jsx
@@ -5,7 +5,7 @@ import MeetingDetails from "./MeetingDetails";
 import { io } from "socket.io-client";
 import Map from "../../Map";
 
-const socket = io("https://healthcare-platform-server.vercel.app/");
+const socket = io("https://healthcare-platform-server.vercel.app");
 
 const IncomingRequest = () => {
   const [appVisible, setAppVisible] = useState(false);
